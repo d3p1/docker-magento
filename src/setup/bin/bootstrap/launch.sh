cc371696cdc134a84b44061a7b06bfe41ea18a03
C. M. de Picciotto <d3p1@d3p1.dev>
2024-04-21 15:18:41 +0000

    refactor: remove init profile from Docker Compose and move logic to launch script [#1]

--- src/setup/bin/bootstrap/launch.sh
+++ src/setup/bin/bootstrap/launch.sh
@@ -29,9 +29,38 @@ main() {
     execute_script "$BASE_DIR/bootstrap/$1/launch.sh"
 
     ##
+    # @todo Remove init profile and execute init commands here
+    ##
+
+    ##
     # @note Return with success
     ##
     return 0
+}
+
+##
+# Init DB
+#
+# @return void
+##
+_init_db() {
+    _normalize_db_dump "$MAGENTO_DB_DUMP_PATH"
+    mysql \
+    -u"$MAGENTO_DB_USER" \
+    -p"$MAGENTO_DB_PASSWORD" \
+    -h"$MAGENTO_DB_HOST" \
+    "$MAGENTO_DB_NAME" \
+    < "$MAGENTO_DB_DUMP_PATH" 
+}
+
+##
+# Normalize DB dump
+#
+# @return void
+# @link   https://github.com/markshust/docker-magento?tab=readme-ov-file#database
+##
+_normalize_db_dump() {
+    sed 's/\sDEFINER=`[^`]*`@`[^`]*`//g' -i src/backup.sql
 }
 
 ##
